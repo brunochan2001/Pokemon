71f4d11e26a621ea4d0c951a65e2ffd04e19a5c3
bruno <brunoacy12@gmail.com>
2022-04-25 20:40:11 -0500

    Agregamos el provider en el app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import SearchPokemon from "./components/search/SearchPokemon";
+import store from "./redux/store";
 
 function App() {
   return (
-    <main className="main ">
-      <section className="py-3">
-        <div className="container">
-          <h1 className="text-center">Buscador de Pokemones</h1>
-          <section className="col-md-12">
-            <div className="row ">
-              <SearchPokemon></SearchPokemon>
-            </div>
-          </section>
-        </div>
-      </section>
-    </main>
+    <Provider store={store}>
+      <main className="main ">
+        <section className="py-3">
+          <div className="container">
+            <h1 className="text-center">Buscador de Pokemones</h1>
+            <section className="col-md-12">
+              <div className="row ">
+                <SearchPokemon></SearchPokemon>
+              </div>
+            </section>
+          </div>
+        </section>
+      </main>
+    </Provider>
   );
 }
 
